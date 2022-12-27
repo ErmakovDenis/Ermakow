551d439aedee58f2081f3bc2b7e78b9420ad7855
ErmakovDenis <clutcherdenis@gmail.com>
2022-12-27 20:51:22 +0500

    Сделал  3.5.1

---
+++ 3.5.1.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import sqlite3
+
+
+def get_sql():
+    """
+    Переводит csv-файл в таблицу sqlite3.
+    """
+    df = pd.read_csv('currency.csv')
+    df.to_sql('currency', sqlite3.connect('currency.sqlite'), if_exists='replace', index=False)
+
+
+get_sql()
